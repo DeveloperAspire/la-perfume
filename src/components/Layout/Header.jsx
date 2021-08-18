84830fbbbba91a068907acc8f940d9a8f95d4703
Franklin <franklinokolie86@gmail.com>
2021-08-18 15:18:17 +0100

    Created a folder to group all the laypout-related components

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Header.module.css'
+
+function Header() {
+      return (
+            <header>
+                 <h1>La Perfume</h1> 
+            </header>
+      )
+}
+
+export default Header
