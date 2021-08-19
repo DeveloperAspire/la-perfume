bde1fba2ee62f72de51afc4915c61eaf0e8099cf
Franklin <franklinokolie86@gmail.com>
2021-08-19 13:25:37 +0100

    Added a new file and modified some styles

---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './CartItem.module.css'
+
+function CartItem() {
+      return (
+            <div className={classes.container}>
+                  <div className={classes.details}>
+                        <img src="../../images/hero.jpg" alt="" />
+                        <h3></h3>
+                  </div>
+                  
+            </div>
+      )
+}
+
+export default CartItem
--- src/components/Products/PerfumeItem.jsx
+++ src/components/Products/PerfumeItem.jsx
@@ -7,6 +7,7 @@ function PerfumeItem() {
       return (
             <div className={classes.product}>
                   <img src={image} alt="" />
+                  <h4 className={classes.name}>Prada</h4>
                    <p className={classes.description}>This is a perfume that vammtiolf shyrruke jkvnbhioarioe kenkfegnek kenfgb;ebj jbfjbJEBGJKBEGJB JJUGHI JRGHRGIIHN jhgjaj.bjhg jenfkg.jj</p>
                    <p className={classes.price}>$20.55</p>
                    <button>Add to cart </button>
