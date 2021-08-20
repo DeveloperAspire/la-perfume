6efa81fb20272b7d62195538797dc8563bb5a5b6
Franklin <franklinokolie86@gmail.com>
2021-08-20 18:20:09 +0100

    Created a new folder to house all Checkout components

---
+++ src/components/CheckOut/CheckOut.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './CheckOut.module.css'
+import CartHeader from '../Cart/CartHeader'
+import CheckOutForm from './CheckOutForm'
+
+function CheckOut({ show, checkOutHandler }) {
+      const checkoutClass = `${classes.checkout} ${show ? classes.show : ''}`
+  return (
+    <div className={checkoutClass}>
+      <CartHeader notShowHandler={checkOutHandler}  title="Checkout" />
+      <CheckOutForm/>
+
+    </div>
+  );
+}
+
+export default CheckOut
---
+++ src/components/CheckOut/CheckOutForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react' 
+
+import classes from './CheckOutForm.module.css'
+
+const CheckOutForm = ()=> {
+
+  const submitHandler = (e)=> {
+       e.preventDefault()
+  }
+
+      return (
+        <form className={classes.form} onSubmit={submitHandler}>
+          <div className={classes["form-item"]}>
+            <div className={classes["form-control"]}>
+              <label htmlFor="Name">Full Name</label>
+              <input type="text" />
+            </div>
+
+            <div className={classes["form-control"]}>
+              <label htmlFor="Email">Email Address</label>
+              <input type="email" />
+            </div>
+
+            <div className={classes["form-control"]}>
+              <label htmlFor="Adress">Billing Address</label>
+              <input type="text" />
+            </div>
+
+            <div className={classes["form-control"]}>
+              <label htmlFor="Zip Code">Zip Code</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          <button className={classes.button} type="submit">
+            Send Order
+          </button>
+        </form>
+      );
+}
+
+export default CheckOutForm
