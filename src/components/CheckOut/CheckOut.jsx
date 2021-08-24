9cbb01c7de3c7cd2557fc7d8c4653fa00244aa67
Franklin <franklinokolie86@gmail.com>
2021-08-24 10:48:35 +0100

    Imported the right files and added the neccsassry logic to make the confirm message show

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './CheckOut.module.css'
 import CartHeader from '../Cart/CartHeader'
 import EditCartButton from '../Buttons/EditCartButton'
 import TotalAmount from "../Layout/TotalAmount";
 import CheckOutForm from './CheckOutForm'
+import Confirm from '../Layout/Confirm'
+
 
 function CheckOut({ show, checkOutHandler, showCartHandler }) {
-      const checkoutClass = `${classes.checkout} ${show ? classes.show : ''}`
+  const [submitted, setSubmitted] = useState(false)
+const checkoutClass = `${classes.checkout} ${show ? classes.show : ''}`
+const showConfirmMessage = ()=> {
+  setSubmitted(true)
+
+  setTimeout(()=>{
+   setSubmitted(false)
+  }, 3000)
+
+  setTimeout(()=> {
+    checkOutHandler()
+  }, 4000)
+}
+       const submitOrder = async (orderDetails) => {
+         const response = await fetch(
+           "https://react-demo-7bdfc-default-rtdb.firebaseio.com/perfume-orders.json",
+           {
+             method: "POST",
+             body: JSON.stringify(orderDetails),
+           }
+         );
+
+         const data = await response.json();
+      showConfirmMessage()
+         console.log(data);
+       };
   return (
+    
     <div className={checkoutClass}>
       <CartHeader notShowHandler={checkOutHandler} title="Checkout" />
-      <CheckOutForm />
+      <Confirm show={submitted}/>
+      <CheckOutForm submitOrder={submitOrder}/>
       <TotalAmount />
       <EditCartButton checkOutHandler={checkOutHandler} showCartHandler={showCartHandler}/>
     </div>
